c14809633d26f72fd7728c3b30b2479c8ed38581
standup8 <borapark13@gmail.com>
2022-02-03 22:16:51 +0900

    Modify: no.7 경우의 수 추가

--- 7/index.js
+++ 7/index.js
@@ -1,7 +1,17 @@
-function solution(n, lost, reserve) {
+function solution(n, losts, reserve) {
   let result = [];
 
-  lost.forEach((lostItem) => {
+  //여벌 체육복이 있는 사람이 체육복을 잃어버린 경우에 대한 조건이 필요함
+  for (let lost of losts) {
+    for (let extra of reserve) {
+      if (lost === extra) {
+        losts = losts.filter((item) => item !== lost);
+        reserve = reserve.filter((item) => item !== extra);
+      }
+    }
+  }
+
+  losts.forEach((lostItem) => {
     reserve.forEach((resItem) => {
       if (lostItem + 1 === resItem || lostItem - 1 === resItem) {
         result.push("T");
@@ -12,5 +22,5 @@ function solution(n, lost, reserve) {
     });
   });
   const findTrueVale = result.filter((res) => res === "T").length;
-  return n - (lost.length - findTrueVale);
+  return n - (losts.length - findTrueVale);
 }
