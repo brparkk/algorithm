8a76d34cd461960a81dae330477943a09ff80860
박보라(Rachel) <borapark13@gmail.com>
2025-02-09 14:55:18 +0000

    feat: pick two and add them

--- coding_test_with_js/array/array_sorting.js
+++ coding_test_with_js/array/array_sorting.js
@@ -39,3 +39,27 @@ function solution(arr) {
     const newArr = [...new Set(arr)];
     return newArr.sort((a, b) => b - a)
 }
+
+/**
+ * ### 문제 03. 두 개 뽑아서 더하기
+ * 정수 배열 numbers가 주어진다.
+ * numbers에서 서로 다른 인덱스에 있는 2개의 수를 뽑아 더해 만들 수 있는 모든 수를 배열에 오름차순으로 담아 반환하는 solution 함수를 구현하세요.
+ * 
+ * # 제약 조건
+ *   - numbers의 길이는 2이상 100 이하
+ *   - numbers의 모든 수 는 0 이상 100이하
+ * 
+ */
+
+function solution(numbers){
+    let newArr = [];
+    for(let i = 0; i< numbers.length; i++){
+        for(let j =i+1; j<numbers.length; j++){
+            newArr.push(numbers[i] + numbers[j]);    
+        }
+    }
+
+    return [...new Set(newArr)].sort((a,b) => a - b);
+}
+
+console.log(solution([2,1,3,4,1])) // [2,3,4,5,6,7]
