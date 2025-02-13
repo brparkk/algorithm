d4c9b845760f7f2e05ac3551c0fbe9ffc39005ec
Rachel <borapark13@gmail.com>
2025-02-13 21:09:04 +0900

    add stack

--- programmers/stack.js
+++ programmers/stack.js
@@ -63,7 +63,7 @@ function solution(decimals) {
 // console.log(solution(27));
 
 /**
- * ### 문제10. 괄호 회전하기
+ * ### 문제10. 괄호 회전하기 ⭐️
  * 다음 규칙을 지키는 문자열을 올바른 괄호 문자열이라고 정의합니다.
  * (), [], {} 는 모두 올바른 괄호 문자열입니다.
  *
@@ -141,6 +141,7 @@ function solution(s) {
  *  - 문자열은 모두 소문자로 이루어져 있습니다.
  */
 
+// 반만 맞음
 function solution(s) {
   let arr = s.split("");
 
@@ -169,3 +170,122 @@ function _solution(s) {
 
   return stack.length === 0 ? 1 : 0;
 }
+
+/**
+ * 문제 12 주식 가격 ⭐️
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42584
+ * 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때,
+ * 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+ *
+ * # 제한 사항
+ *  - prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
+ *  - prices의 길이는 2 이상 100,000 이하입니다.
+ *
+ * 입출력 예
+ * prices	            return
+ * [1, 2, 3, 2, 3]	[4, 3, 1, 1, 0]
+ *
+ * 입출력 예 설명
+ * 1초 시점의 ₩1은 끝까지 가격이 떨어지지 않았습니다.
+ * 2초 시점의 ₩2은 끝까지 가격이 떨어지지 않았습니다.
+ * 3초 시점의 ₩3은 1초뒤에 가격이 떨어집니다. 따라서 1초간 가격이 떨어지지 않은 것으로 봅니다.
+ * 4초 시점의 ₩2은 1초간 가격이 떨어지지 않았습니다.
+ * 5초 시점의 ₩3은 0초간 가격이 떨어지지 않았습니다.
+ */
+
+//시간초과... :(
+function solution(prices) {
+  const n = prices.length;
+  const answer = [];
+
+  for (let i = 0; i < n; i++) {
+    let count = 0;
+    for (let j = i + 1; j < n; j++) {
+      if (prices[i] <= prices[j]) {
+        count += 1;
+      }
+    }
+    answer.push(count);
+  }
+
+  return answer;
+}
+
+// console.log(solution([1, 2, 3, 2, 3]));
+
+// stack 활용
+function _solution(prices) {
+  const n = prices.length;
+  const answer = new Array(n).fill(0);
+
+  const stack = [0];
+  for (let i = 1; i < n; i++) {
+    while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+      // 주식 가격이 떨어지는 순간
+      const j = stack.pop();
+      answer[j] = i - j;
+    }
+    stack.push(i);
+  }
+
+  while (stack.length > 0) {
+    const j = stack.pop();
+    answer[j] = n - 1 - j;
+  }
+
+  return answer;
+}
+
+/**
+ * 게임개발자인 "죠르디"는 크레인 인형뽑기 기계를 모바일 게임으로 만들려고 합니다.
+ * "죠르디"는 게임의 재미를 높이기 위해 화면 구성과 규칙을 다음과 같이 게임 로직에 반영하려고 합니다.
+ *
+ * 게임 화면은 "1 x 1" 크기의 칸들로 이루어진 "N x N" 크기의 정사각 격자이며
+ * 위쪽에는 크레인이 있고 오른쪽에는 바구니가 있습니다. (위 그림은 "5 x 5" 크기의 예시입니다).
+ *
+ * 각 격자 칸에는 다양한 인형이 들어 있으며 인형이 없는 칸은 빈칸입니다.
+ * 모든 인형은 "1 x 1" 크기의 격자 한 칸을 차지하며 격자의 가장 아래 칸부터 차곡차곡 쌓여 있습니다.
+ * 게임 사용자는 크레인을 좌우로 움직여서 멈춘 위치에서 가장 위에 있는 인형을 집어 올릴 수 있습니다.
+ *
+ * 집어 올린 인형은 바구니에 쌓이게 되는 데, 이때 바구니의 가장 아래 칸부터 인형이 순서대로 쌓이게 됩니다.
+ * 다음 그림은 [1번, 5번, 3번] 위치에서 순서대로 인형을 집어 올려 바구니에 담은 모습입니다.
+ * 
+ * 게임 화면의 격자의 상태가 담긴 2차원 배열 board와 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열 moves가 매개변수로 주어질 때, 크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+* [제한 사항]
+- board의 각 칸에는 0 이상 100 이하인 정수가 담겨있습니다.
+- board 배열은 2차원 배열로 크기는 "5 x 5" 이상 "30 x 30" 이하입니다.
+- 0은 빈 칸을 나타냅니다.
+- 1 ~ 100의 각 숫자는 각기 다른 인형의 모양을 의미하며 같은 숫자는 같은 모양의 인형을 나타냅니다.
+- moves 배열의 크기는 1 이상 1,000 이하입니다.
+- moves 배열 각 원소들의 값은 1 이상이며 board 배열의 가로 크기 이하인 자연수입니다.
+ */
+
+function solution(board, moves) {
+  const stack = [];
+
+  for (let i = 0; i < moves.length; i++) {
+    const rowIdx = i - 1;
+    const rows = board.map((b) => b[rowIdx]).filter((d) => d !== 0);
+    console.log(rows, "rows");
+
+    if (stack.length > 0 && stack[stack.length - 1] === rows[0]) {
+      stack.pop();
+      rows.shift();
+    } else {
+      stack.push(rows[0]);
+    }
+  }
+
+  return stack.length;
+}
+
+const $board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+const $moves = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution($board, $moves));
